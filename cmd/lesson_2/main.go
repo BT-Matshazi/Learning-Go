85acf9bbfa3209c1ded7d132e75e4d4a55cccc87
BT-Matshazi <bekithembamatshazi@gmail.com>
2024-09-30 23:55:31 +0200

    day 2 of go

--- cmd/lesson_2/main.go
+++ cmd/lesson_2/main.go
@@ -1,13 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("my fav number is", rand.Intn(10))
 
-	message := []string{
-		"hey", "you",
-	}
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
 
-	fmt.Println(len(message))
+	fmt.Println("Value of Pi is: ", math.Pi)
+
+	fmt.Println(add(43, 20))
+
+	fmt.Println(addTwo(77, 55))
+	
+	fmt.Println(swap("s", "b"))
+
+	fmt.Println(split(12))
+}
+
+// type comes after variable
+func add( x int, y int) int {
+	return x + y
+}
+
+// is arguments that follow each other share a type, you cant omit
+func addTwo(x , y int) int {
+	return x + y
+}
+
+// function that return 2 value
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int){
+	x = sum * 4 / 2
+	y = sum - x
+	return
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module variables
 
 go 1.22.4
 
-require github.com/aws/aws-lambda-go v1.47.0
+require (
+	github.com/aws/aws-lambda-go v1.47.0
+	rsc.io/quote v1.5.2
+)
+
+require (
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
